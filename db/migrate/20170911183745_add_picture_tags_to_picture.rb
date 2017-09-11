40c9d5eb1c3e0083b2fdccc0889ea4baa14ce748
sophiehelf <sophie.helf@gmail.com>
2017-09-11 14:48:07 -0400

    created associations and seeded database

---
+++ db/migrate/20170911183745_add_picture_tags_to_picture.rb
@@ -0,0 +1,4 @@
+class AddPictureTagsToPicture < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+[1,2,3].each do |num|
+  User.create(
+    username:"user#{num}",
+    email: "user#{num}@example.com",
+    password: "test123"
+  )
+end
+
+["Nature", "NYC", "Art", "Humor"].each do |tag_name|
+  Tag.create(name: tag_name)
+end
+
+url = "http://www.defenders.org/sites/default/files/styles/large/public/dolphin-kristian-sekulic-isp.jpg"
+
+Picture.create(
+  image_url: url,
+  title: "Saw a dolphin!",
+  user_id: User.first.id
+)
