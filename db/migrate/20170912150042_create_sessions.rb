4af315c69976f336d2b9f898e74285638040bdbd
sophiehelf <sophie.helf@gmail.com>
2017-09-12 13:08:35 -0400

    reorganized database

--- db/migrate/20170912150042_create_sessions.rb
+++ db/migrate/20170912150042_create_sessions.rb
@@ -2,7 +2,6 @@ class CreateSessions < ActiveRecord::Migration[5.1]
   def change
     create_table :sessions do |t|
       t.integer :user_id
-      t.string :password_digest
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20170912150042) do
 
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id"
-    t.string "password_digest"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -49,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170912150042) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
